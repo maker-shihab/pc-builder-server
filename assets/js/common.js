9a2321a32fefeab7ede39fde08a55959fde5f2c2
Shihab <73121069+maker-shihab@users.noreply.github.com>
2021-12-05 11:39:42 +0600

    some code update

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,3 +1,37 @@
-const test = n => n
+$(document).ready(function(){
+  $('.faq_header').click(function(){
+    $(this).toggleClass('active');
+    $(this).parent('.singleFaq__item').toggleClass('active');
+  });
 
-test(12)
+  $('.mobileMenu').click(function(){
+    $(this).toggleClass("active");
+    $('.primaryMenu').toggleClass("active");
+  });
+  $('.menuLInk').click(function(){
+    $('.primaryMenu').removeClass("active");
+    $('.mobileMenu').removeClass("active");
+  }); 
+
+
+  $('.d-b-d').click(function(){  
+		$('html, body').animate({    
+			scrollTop:$($(this).attr('href')).offset().top
+		}, 1000);
+		$(".menu").removeClass("menu_active");
+		$(".menu_bars").removeClass("open");
+
+	return false;
+	});
+});
+
+$(window).scroll(function(){
+	var scrollvalue = $(this).scrollTop();
+		// back-to-top
+  	if(scrollvalue>100){
+        $('header').addClass('active');
+    }else{
+        $('header').removeClass('active');
+	}
+	return false;
+}); 
